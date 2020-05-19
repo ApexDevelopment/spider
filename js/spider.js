f295a44a804f3afc7b53057a9615ce6a5ef047ce
Apex <micah.apex@gmail.com>
2020-05-19 12:33:12 -0400

    toolbar

--- js/spider.js
+++ js/spider.js
@@ -1,7 +1,39 @@
-/* global document, Terminal */
+/* jshint esversion: 6 */
+/* global Terminal */
 
+console.log("Spider init");
+
+let { remote } = require("electron");
 let $ = require("jquery");
 
-let term = new Terminal();
-term.open(document.getElementById("terminal"));
-term.write("Hello from Spider.");
+$("#close").click(() => {
+	let window = remote.getCurrentWindow();
+	window.close();
+});
+
+$("#max").click(() => {
+	let window = remote.getCurrentWindow();
+
+	if (!window.isMaximized()) {
+		window.maximize();
+	}
+	else {
+		window.unmaximize();
+	}
+});
+
+$("#min").click(() => {
+	let window = remote.getCurrentWindow();
+	window.minimize();
+});
+
+console.log("Done");
+
+/*let term = new Terminal({
+	theme: {
+		background: "#2e3440"
+	}
+});*/
+
+//term.open(document.getElementById("terminal"));
+//term.write("Hello from Spider.");
--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
 	let window = new BrowserWindow({
+		backgroundColor: "#2e3440",
+		titleBarStyle: "hidden",
+		title: "Spider",
+		frame: false,
+		// transparent: process.platform === "darwin",
 		width: 800,
 		height: 600,
 		webPreferences: {
