c5ed2e9cf598342a58303805db57cbdf2cb158fb
Apex <micah.apex@gmail.com>
2020-05-21 13:24:42 -0400

    Display both nickname and tag

--- js/format.js
+++ js/format.js
@@ -1,9 +1,10 @@
 /* jshint esversion: 6*/
-let color = require("./color");
+const nord = require("./nord.js");
+let color = require("./color.js");
 
 module.exports = {
 	formatMessage: function(message) {
 		let nordColor = `#${color.toNordPalette(message.member.displayHexColor, true)}`;
-		return `{color=${nordColor}}<${message.author.tag}>{/color} ${message.cleanContent}`;
+		return `{color=${nordColor}}<${message.member.displayName}>{/color} {color=#${nord[4]}}(${message.author.tag}){/color} ${message.cleanContent}`;
 	}
 };
