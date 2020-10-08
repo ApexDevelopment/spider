5455f659a332a43a014c8f87563166505e161dfa
Micah Havens <micah.apex@gmail.com>
2020-10-08 15:30:44 -0400

    Make message formatting nicer and more flexible

--- js/format.js
+++ js/format.js
@@ -2,9 +2,41 @@
 const nord = require("./nord.js");
 let color = require("./color.js");
 
+// TODO: This script could use cleanup
 module.exports = {
-	formatMessage: function(message) {
+	// TODO: Use an options object as an argument to manage how formatting works
+	formatMessage: function(message, includeMetadata = true, rich = true) {
+		const date = message.createdAt;
 		let nordColor = `#${color.toNordPalette(message.member.displayHexColor, true)}`;
-		return `{color=${nordColor}}<${message.member.displayName}>{/color} {color=#${nord[4]}}(${message.author.tag}){/color} ${message.cleanContent}`;
+		let formatted = `${message.cleanContent}`;
+
+		if (message.attachments.size > 0) {
+			formatted += `\n${rich ? `{color=#${nord[7]}}` : ""}<Attachments>${rich ? "{/color}" : ""}`;
+
+			if (rich) {
+				message.attachments.each(attachment => formatted += `\n{link=${attachment.proxyURL}}${attachment.name ? attachment.name : "Unnamed"}{/link}`);
+			}
+			else {
+				message.attachments.each(attachment => formatted += `\n[${attachment.name ? attachment.name : "Unnamed"}] ${attachment.proxyURL}`);
+			}
+		}
+
+		formatted = formatted.trim();
+		
+		if (includeMetadata) {
+			let meta = "";
+			
+			if (rich) meta += `{color=${nordColor}}`;
+			meta += `<${message.member.displayName}> `;
+			if (rich) meta += `{/color}{color=#${nord[3]}}`;
+			meta += `(${message.author.tag}) `
+				+ `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()} at ${date.toLocaleTimeString()}`;
+			if (rich) meta += `{/color}`;
+			meta += "\n";
+
+			formatted = meta + formatted;
+		}
+
+		return formatted;
 	}
 };
